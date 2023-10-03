ed02e8370b589ffe1a00d58bc21d80be441023a0
aungpyaephyo1412 <aungpyaephyo1412@gmail.com>
2023-10-03 17:35:54 +0630

    read done

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $items = Item::when(\request()->has("keyword"), function (Builder $query) {
+            $query->where("name","like","%".request()->keyword."%");
+        })->paginate(10)->withQueryString();
+        return response()->json($items,200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
