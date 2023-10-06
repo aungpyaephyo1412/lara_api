beae21f90e14d531523dd259376d92a64246f8ae
aungpyaephyo1412 <aungpyaephyo1412@gmail.com>
2023-10-06 12:18:19 +0630

    route define

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function login()
+    {
+
+    }
+
+    public function check()
+    {
+
+    }
+
+    public function logOut()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix("auth")->controller(AuthController::class)->group(function (){
+    Route::get("register","register")->name("auth.register");
+    Route::post("register","store")->name("auth.store");
+    Route::get("login","login")->name("auth.login");
+    Route::post("login","check")->name("auth.check");
+    Route::post("logout","logOut")->name("auth.logout");
+});
+
+Route::prefix("dashboard")->controller(HomeController::class)->group(function (){
+    Route::get("home","home")->name("dashboard.home");
 });
